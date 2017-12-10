00ba3826a1e6dfcc518bff9fdffef5897c789cd5
ccepe014 <ccepe014@fiu.edu>
2017-12-09 23:19:37 -0500

    I was able to parallelize the program, by making the Player class. Some of the output will need to be copied from the other program into the new one. And we still need to finish parallelizing the other things.

--- src/blackjack2/BlackJack2.java
+++ src/blackjack2/BlackJack2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package blackjack2;
 
 import java.util.Scanner;
@@ -10,54 +5,65 @@ import java.util.Vector;
 import java.util.List;
 import java.util.ArrayList;
 
-
-
-
-
 /**
- *
  * @author Hornets
  */
  public class BlackJack2 {
    /** InitializeVaraiable */
-   private static List<Player> playersPlaying = new ArrayList<Player>();
    private static Scanner scanner = new Scanner(System.in);
+   private static List<Player> playersPlaying = new ArrayList<Player>();
+   private static int[] totalDeck; // WE WILL STORE ALL THE DECKS NEEDED IN HERE
+   private final static int numOfDecks = 6; // THIS NUMBER IS HARDCODED IT WOULD BE BETTER IF ITS NOT
+   private static int currentPosition;
 
-
-
-   static class Player extends Thread {
+   static  class Player extends Thread {
      // The only variable that really defines a player is his hand
-     private Vector userHand;
+     private final String TYPE_OF_PLAYER;
+     private Vector userHand = new Vector();
 
      // Variable needed to getCard
-     private int PLAYER_NUMBER;
-     private int NUM_OF_PLAYERS;
+     private final int PLAYER_NUMBER;
+     private final int NUM_OF_PLAYERS;
 
-     Player(int playerNumber, int numberOfPlayers){
-       PLAYER_NUMBER = playerNumber;
-       NUM_OF_PLAYERS =numberOfPlayers;
+     Player(int playerNumber, int numberOfPlayers, String type){
+         PLAYER_NUMBER = playerNumber;
+         NUM_OF_PLAYERS =numberOfPlayers;
+         TYPE_OF_PLAYER = type;
      }
 
      /* Essentially run will be the method playBlackJack()
         Step 1 - Get the two cards
         Step 2 - Add its card count
         */
+     @Override
      public void run(){
-         System.out.println("Player Number: " + (PLAYER_NUMBER+1) + " was just created.");
+         //System.out.println("Player Number: " + (PLAYER_NUMBER+1) + " was just created.");
          try{
              getCards(PLAYER_NUMBER,NUM_OF_PLAYERS);
-             Thread.sleep(3000); // 1 SECOND = 1000 MILLISECONDS
+             //System.out.println("Player Number: " + (PLAYER_NUMBER+1) + " card 1: " + showCard((Integer)userHand.elementAt(0)) + " card 2: "+ showCard((Integer)userHand.elementAt(1)));
+             Thread.sleep(0); // 1 SECOND = 1000 MILLISECONDS
          } catch(InterruptedException e){
              System.out.println("Player Number: " + (PLAYER_NUMBER+1) + " interrupted.");
          } // END CATCH
-         System.out.println("Player Number: " + (PLAYER_NUMBER+1) + " done playing.");
-
+         //System.out.println("Player Number: " + (PLAYER_NUMBER+1) + " done playing.");
      } // END OF RUN() FUNCTION
-     public static void getCards(int playerNumber, int totalNumberOfPlayers) {
+     public Vector getUserHand(){
+       return userHand;
+     }
+     public void getCards(int playerNumber, int totalNumberOfPlayers) {
+         userHand.addElement(getArrayCard(playerNumber));
+         userHand.addElement(getArrayCard((playerNumber+totalNumberOfPlayers)));
        // This method is suppose to get the cards from the Array of Cards
      }
-
-   }
+     public static void standOrHit(String typeOfPlayer){
+       // This function should determine if you want to hit or stand if you
+       // are the user you have to wait for resond VS being the computer that
+       // it will be the AI that determins if it hits or stands
+     }
+     public static int getArrayCard(int index){
+       return BlackJack2.totalDeck[index];
+     }
+   } // END PLAYER CLASS
 
    public static void main(String[] args) {
      /* You had this as an idea
@@ -68,7 +74,6 @@ import java.util.ArrayList;
 
    }
 
-
    /** THINGS YOU NEED
     *   playBlackJack()
     *       - This method should
@@ -76,8 +81,9 @@ import java.util.ArrayList;
     *         the conversion would be for every 10 player we would need 1 decks
    */
    public static void playBlackJack(){
-       int[] totalDeck; // WE WILL STORE ALL THE DECKS NEEDED IN HERE
        int totalNumberOfPlayers;
+       String COMPUTER = "Computer";
+       String USER = "User";
 
        /* NEEDED SO WE CAN SEE HOW LONG THE PROGRAM RAN FOR */
        long startTimeOfProgram, endTimeOfProgram, totalTimeOfProgram;
@@ -93,7 +99,7 @@ import java.util.ArrayList;
        endTimeOfUserRespond = System.currentTimeMillis();
        totalTimeOfUserRespond = endTimeOfUserRespond - startTimeOfUserRespond;
 
-       System.out.println("Number of players entered are " + numOfCompPlayers);
+       // System.out.println("Number of players entered are " + numOfCompPlayers);
 
        /*********************** CREATE DECK OF CARDS ****************************/
 
@@ -102,7 +108,6 @@ import java.util.ArrayList;
        // ADDED TO THE PROGRAM
 
        int cardCt = 0;
-       int numOfDecks = 6;
        totalDeck = new int[52 * numOfDecks];
 
        // Loop through the numOfDecks that we decide to insert into the Blackjack Game
@@ -116,40 +121,194 @@ import java.util.ArrayList;
            }
          }
        }
-       /*************************************************************************/
+       shuffle();
+       /***********************************************************************/
+
+       /**************** THIS IS THE PART THAT IS THREADED ********************/
+       // Plus 1 becasue of the user
+       totalNumberOfPlayers = numOfCompPlayers + 1;
+       // THIS IS TO ADD US AS THE USER SO IT CAN BECOME A THREAD
+       playersPlaying.add(new Player(0,totalNumberOfPlayers,USER));
+       playersPlaying.get((0)).run();
+
+       // THIS FOR LOOP IS TO ADD THE COMPUTERS INTO THE VECTOR AND THEN SO
+       // THEY CAN ALSO BECOME THREADS
+       for (int playerNUMBER = 1; playerNUMBER <= numOfCompPlayers; playerNUMBER++) {
+           playersPlaying.add(new Player(playerNUMBER,totalNumberOfPlayers,COMPUTER));
+           playersPlaying.get(playerNUMBER).run();
+       }
+       /***********************************************************************/
+
+       printComputerPlayerHand();
 
 
+
+       /********************** DONT MOVE THIS FROM HERE **********************/
        endTimeOfProgram   = System.currentTimeMillis();
        totalTimeOfProgram = endTimeOfProgram - startTimeOfProgram - totalTimeOfUserRespond;
        System.out.println("The Program took this long " + totalTimeOfProgram);
-
-       // Plus 1 becasue of the user
-       totalNumberOfPlayers = numOfCompPlayers + 1;
-
-       // THIS FOR LOOP IS TO ADD THEM INTO THE VECTOR
-       for (int playerON = 0; playerON < totalNumberOfPlayers; playerON++) {
-           playersPlaying.add(new Player(playerON,totalNumberOfPlayers));
-           playersPlaying.get((playerON)).run();
-       }
-
-//       // THIS FOR LOOP IS MEANT TO START THEM
-//       for (int i = 0; i < 10; i++) {
-//          playersPlaying.get(2).run();
-//       }
-//
-//
-//
-//       playersPlaying.get(3).run();
-
-
-
      } //  END OF playBlackJack()
+   public static void shuffle() {
+     // Put all the used cards back into the deck, and shuffle it into
+     // a random order.
+     for (int i = (52 * numOfDecks - 1); i > 0; i--) {
+       int rand = (int) (Math.random() * (i + 1));
+       int temp = totalDeck[i];
+       totalDeck[i] = totalDeck[rand];
+       totalDeck[rand] = temp;
+     }
+     currentPosition = 0;
+   } // END OF shuffle() FUNCTION
+   // MAYBE SO THAT WE CAN GET EACH PLAYER CARD COUNT WE CAN PASS IT AS A PARAMETER
+   public static void printComputerPlayerHand() {
+     System.out.println("");
+     System.out.printf("%s %14s %16s %18s", "Player", "Total", "Cards", "CardCount\n");
+     System.out.println("-------------------------------------------------------\n");
+
+     /******************** PRINT US AS THE FIRST USER  ************************/
+     // WE ARE INDEX 0(ZERO) OF THE ARRAYLIST THAT HOLDS THE PLAYERS
+     System.out.printf("%d\t\t", 1);
+
+     // THIS SHOULD RETURN THE VALUE OF THE CARDS THAT WE ARE HOLDING
+     // As of now this returns a player object
+     // and the value that needs to be given into the method value(Vector hand)
+     System.out.printf("%d\t\t", value( playersPlaying.get(0).getUserHand() ));
+
+     // THIS SHOULD PRINT OUT ALL THE CARDS THAT WE ARE HOLDING
+     for (int cardON = 0; cardON < playersPlaying.get(0).getUserHand().size(); cardON++) {
+       if (cardON == playersPlaying.get(0).getUserHand().size() - 1) {
+         System.out.printf("%s\t\t", showCard(getCard(playersPlaying.get(0).getUserHand(), cardON)));
+       } else {
+         System.out.printf("%s, ", showCard(getCard(playersPlaying.get(0).getUserHand(), cardON)));
+       }
+     }
+     System.out.println("");
+     /*************************************************************************/
+
+
+
+
+     /*********************** PRINT THE COMPUTERS *****************************/
+     for (int playerON = 1; playerON < playersPlaying.size(); playerON++) {
+       // THIS IS THE NUMBER OF THE PLAYER
+       // EXAMPLE PLAYER 2 IS COMPUTER NUMBER 1
+       //         PLAYER 3 IS COMPUTER NUMBER 2
+       System.out.printf("%d\t\t", playerON + 1);
+       // THIS IS THE SUM OF BOTH CARD VALUES
+       System.out.printf("%d\t\t", value( playersPlaying.get(playerON).getUserHand() ));
+
+       // THIS IS TO PRINT THE PLAYER HAND SO IT WILL LOOP THROUGH THE EACH
+       // INDEX IN THE VECTOR AND PRINT EACH CARD OUT
+       for (int cardON = 0; cardON < playersPlaying.get(playerON).getUserHand().size(); cardON++) {
+         if (cardON == playersPlaying.get(playerON).getUserHand().size() - 1) {
+           System.out.printf("%s\t\t", showCard(getCard(playersPlaying.get(playerON).getUserHand(), cardON)));
+         } else {
+           System.out.printf("%s, ", showCard(getCard(playersPlaying.get(playerON).getUserHand(), cardON)));
+         }
+       }
+       /***********************************************************************/
+
+       // ADDED THIS IN HOPE THAT IT WOULD PRINT EACH PLAYER TOTAL CARD AS THEY GOT THERE CARDS
+       //System.out.println(totalCardCount);
+       System.out.println("");
+     }
+     System.out.println("\n-------------------------------------------------------");
+   }
+   public static String showCard(int card) {
+       switch (card)
+       {
+         case 1:
+         return "Ace";
+         case 2:
+         return "2";
+         case 3:
+         return "3";
+         case 4:
+         return "4";
+         case 5:
+         return "5";
+         case 6:
+         return "6";
+         case 7:
+         return "7";
+         case 8:
+         return "8";
+         case 9:
+         return "9";
+         case 10:
+         return "10";
+         case 11:
+         return "Jack";
+         case 12:
+         return "Queen";
+         case 13:
+         return "King";
+         default:
+         return "??";
+       }
+     }
+   public static int value(Vector hand) {
+     // Returns the value of this hand for the
+     // game of Blackjack.
+
+     int val;      // The value computed for the hand.
+     boolean ace;  // This will be set to true if the
+     //   hand contains an ace.
+     int cards;    // Number of cards in the hand.
+
+     val = 0;
+     ace = false;
+     cards = hand.size();
+
+     for (int i = 0; i < cards; i++) {
+       // Add the value of the i-th card in the hand.
+       int card; // The i-th card;
+       int cardVal; // The blackjack value of the i-th card.
+       card = getCard(hand, i);
+       cardVal = getCardValue(card); // The normal value, 1 to 13.
+       if (cardVal > 10) {
+         cardVal = 10; // For a Jack, Queen, or King.
+       } else if (cardVal == 1) {
+         ace = true; // There is at least one ace.
+       }
+       val = val + cardVal;
+     }
+
+     /*  SINCE WE ARE CHECKING IF THERE HAS BEEN AN ACE THEN WE JUST ADD 10 TO
+         THE VALUE OF 1 */
+     // Now, val is the value of the hand, counting any ace as 1.
+     // If there is an ace, and if changing its value from 1 to
+     // 11 would leave the score less than or equal to 21,
+     // then do so by adding the extra 10 points to val.
+     if (ace == true && val + 10 <= 21){
+       val = val + 10;
+     }
+     return val;
+   }
+   public static int getCard(Vector hand, int position) {
+     // Get the card from the hand in given position, where positions
+     // are numbered starting from 0.  If the specified position is
+     // not the position number of a card in the hand, then null
+     // is returned.
+     if (position >= 0 && position < hand.size())
+     {
+       return ((Integer) hand.elementAt(position)).intValue();
+     }
+     else
+     {
+       return 0;
+     }
+   }
+   public static int getCardValue(int card) {
+     int result = card;
+     switch (card)
+     {
+       case 11:
+       case 12:
+       case 13:
+       result = 10;
+     }
+     return result;
    }
 
-/**
-ArrayList<Players> x = new ArrayList<Players>();
-for (int i=0 ; i<10; i++) {
-  Player y = new Player();
-  x.addElement(y);
-}
-*/
+ } // END OF BlackJack2 CLASS
